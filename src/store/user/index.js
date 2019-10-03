6f5e687fdadfbdea5345f15f4babb7abcef5c827
Anuarder <anuar.ibraev97@gmail.com>
2019-10-03 18:51:51 +0600

    Login services

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,9 +1,14 @@
+import AuthServices from "@/services/Auth"
+
 const state = {
     token: null,
+    login_errors: null,
+    login_loading: false,
     user: {
         role: 'admin'
     },
 }
+
 const getters = {
     GET_TOKEN(state){
         return state.token;
@@ -12,8 +17,33 @@ const getters = {
         return state.user;
     }
 }
-const mutations = {}
-const actions = {}
+
+const mutations = {
+    SET_USER(state, user){
+        state.user = user
+    },
+    SET_LOADING(state, loading){
+        state.loading = loading;
+    }
+}
+
+const actions = {
+    async LOGIN({ commit }, payload){
+        try{
+            commit('SET_LOADING', true)
+            const response = await AuthServices.login(payload);
+            if(response.data.status){
+                commit('SET_USER', response.data.user);
+                commit('SET_LOADING', false);
+            }else{
+                console.log('fuck')
+                commit('SET_LOADING', false);
+            }
+        }catch(err){
+            console.log(err);
+        }
+    }
+}
 
 export default {
     state, getters, mutations, actions
