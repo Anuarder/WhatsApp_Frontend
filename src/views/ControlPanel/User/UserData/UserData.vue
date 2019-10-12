cf6fd2519d3289e71a9ed87343944566d8bea6ac
Anuarder <anuar.ibraev97@gmail.com>
2019-10-13 01:17:01 +0600

    User update data

--- src/views/ControlPanel/User/UserData/UserData.vue
+++ src/views/ControlPanel/User/UserData/UserData.vue
@@ -10,11 +10,11 @@
                                 v-if="isEdit"
                                 type="text"  
                                 class="create__input__element" 
-                                v-model="first_name">
+                                v-model="editable_fields.first_name">
                             <span 
                                 v-else 
                                 class="create__input__element">
-                                {{first_name}}
+                                {{user.first_name}}
                             </span>
                         </div>
                     </div>
@@ -25,11 +25,11 @@
                                 v-if="isEdit" 
                                 type="text"  
                                 class="create__input__element" 
-                                v-model="last_name">
+                                v-model="editable_fields.last_name">
                             <div 
                                 v-else 
                                 class="create__input__element">
-                                {{last_name}}
+                                {{user.last_name}}
                             </div>
                         </div>
                     </div>
@@ -40,11 +40,11 @@
                                 v-if="isEdit" 
                                 type="text"  
                                 class="create__input__element" 
-                                v-model="email">
+                                v-model="editable_fields.email">
                             <span 
                                 v-else 
                                 class="create__input__element">
-                                {{email}}
+                                {{user.email}}
                             </span>
                         </div>
                     </div>
@@ -55,11 +55,11 @@
                                 v-if="isEdit" 
                                 type="text"  
                                 class="create__input__element" 
-                                v-model="phone" >
+                                v-model="editable_fields.phone" >
                             <span 
                                 v-else 
                                 class="create__input__element">
-                                {{phone}}
+                                {{user.phone}}
                             </span>
                         </div>
                     </div>
@@ -68,11 +68,17 @@
                         class="create__buttons">
                         <button 
                             class="w-button" 
-                            @click="isEdit = false">
-                            Сохранить
+                            @click="edit">
+                            <span v-if="isLoading">
+                                Загрузка...
+                            </span>
+                            <span v-else>
+                                Сохранить
+                            </span>
                         </button>
                         <button 
-                            class="w-button w-button__outline">
+                            class="w-button w-button__outline"
+                            @click="isEdit = false">
                             Отмена
                         </button>
                     </div>
@@ -80,8 +86,8 @@
             </div>
             <div 
                 class="create__actions" 
-                v-if="!isEdit"
-                @click="isEdit = true">
+                v-if="!isEdit" 
+                @click="allow_editing">
                 <img src="@/assets/images/edit.svg" alt="edit">
                 Редактировать
             </div>
@@ -89,14 +95,40 @@
     </div>
 </template>
 <script>
+import { mapGetters } from "vuex"
 export default {
     data(){
         return{
-            first_name: 'Арман',
-            last_name: 'Тажибаев',
-            email: 'armant89@mail.ru',
-            phone: '+7 777 366 66 66',
-            isEdit: false
+            editable_fields: null,
+            isEdit: false,
+            isLoading: false
+        }
+    },
+    computed: {
+        ...mapGetters(['GET_USER']),
+        user(){
+            return this.GET_USER;
+        }
+    },
+    methods: {
+        allow_editing(){
+            this.editable_fields = {
+                ...this.user
+            }
+            this.isEdit = true;
+        },
+        async edit(){
+            try{
+                this.isLoading = true;
+                const response = await this.$store.dispatch('EDIT_USER', this.editable_fields);
+                if(response.status){
+                    this.isLoading = false;
+                }else{
+                    throw response;
+                }
+            }catch(err){
+                console.log(err);
+            }
         }
     }
 }
